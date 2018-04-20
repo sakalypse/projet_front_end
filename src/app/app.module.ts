639772e689b87472fff73f26dc87d272ed145f8f
kevin.limacher <kevin.limacher@gmail.com>
2018-04-20 16:48:05 -0400

    add import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { FontAwesomeModule } from 'ngx-icons';
 import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data-service';
 
 @NgModule({
   declarations: [
