8b91029c1dea8a07a60b5ce5c7a7e63ff5826ab8
kevin.limacher <kevin.limacher@gmail.com>
2018-04-20 16:51:56 -0400

    add import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,8 +26,7 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
     MatButtonModule,
     FontAwesomeModule,
     HttpClientModule,
-    HttpClientInMemoryWebApiModule,
-    InMemoryDataService
+    HttpClientInMemoryWebApiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
